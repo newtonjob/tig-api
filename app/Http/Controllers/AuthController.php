acc9a8c053dabde63021a592df5b906414d15331
newtonjob <jobnewton3@gmail.com>
2023-05-08 20:57:57 +0100

    Implement auth token endpoint

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Http;
+
+class AuthController extends Controller
+{
+    public function __invoke(Request $request)
+    {
+        $response = Http::tig()->get('/', [
+            'action'   => 0,
+            'email_id' => $request->email,
+            'pass_key' => $request->password,
+        ])->object();
+
+        if ($response->ex_resp_code != 0) {
+            return Response::api($response->ex_resp_desc, status: 422);
+        }
+
+        return Response::api($response->ex_resp_desc, [
+            'token' => "{$response->ex_ref_no}|{$response->sessionID}"
+        ]);
+    }
+}
