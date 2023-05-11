3c878bc0f05362cd191696294fb97109f8abd02a
newtonjob <jobnewton3@gmail.com>
2023-05-11 10:24:28 +0100

    Create AirtimeAndDataRequest

--- app/Http/Controllers/AirtimeController.php
+++ app/Http/Controllers/AirtimeController.php
@@ -2,34 +2,12 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use Illuminate\Http\Response;
-use Illuminate\Support\Facades\Http;
+use App\Http\Requests\AirtimeAndDataRequest;
 
 class AirtimeController extends Controller
 {
-    public function __invoke(Request $request)
+    public function __invoke(AirtimeAndDataRequest $request)
     {
-        $response = Http::tig()->get('/', [
-            'action'       => 1,
-            'ex_ref_no'    => $request->user()->ref_no,
-            'sessionID'    => $request->user()->id,
-            'vend_email'   => $request->user()->email,
-            'trans_amount' => $request->amount,
-            'bill_id'      => $request->bill_id,
-            'item_id'      => $request->item_id,
-            'phone_numb'   => $request->phone,
-        ])->object();
-
-        if ($response->ex_resp_code != 90000) {
-            return Response::api($response->ex_resp_desc, status: 422);
-        }
-
-        return Response::api($response->ex_resp_desc, [
-            'reference'     => $response->tnx_ref,
-            'vtu_reference' => $response->vtu_ref,
-            'type'          => $response->trans_type,
-            'amount'        => $response->trans_amount,
-        ]);
+        return $request->fulfill(1);
     }
 }
---
+++ app/Http/Requests/AirtimeAndDataRequest.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Http;
+
+class AirtimeAndDataRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     */
+    public function rules(): array
+    {
+        return [
+            'amount'  => 'required',
+            'bill_id' => 'required',
+            'item_id' => 'required',
+            'phone'   => 'required',
+        ];
+    }
+
+    /**
+     * Fulfill the request.
+     */
+    public function fulfill($action)
+    {
+        $response = Http::tig()->get('/', [
+            'action'     => $action,
+            'ex_ref_no'  => $this->user()->ref_no,
+            'sessionID'  => $this->user()->id,
+            'vend_email' => $this->user()->email,
+            'trans_amt'  => $this->amount,
+            'bill_id'    => $this->bill_id,
+            'item_id'    => $this->item_id,
+            'phone_numb' => $this->phone,
+        ])->json(0);
+
+        if ($response['ex_resp_code'] != 90000) {
+            return Response::api($response['ex_resp_desc'], status: 422);
+        }
+
+        return Response::api($response['ex_resp_desc'], [
+            'reference'     => $response['tnx_ref'],
+            'vtu_reference' => $response['vtu_ref'],
+            'type'          => $response['trans_type'],
+            'amount'        => $response['trans_amount'],
+        ]);
+    }
+}
